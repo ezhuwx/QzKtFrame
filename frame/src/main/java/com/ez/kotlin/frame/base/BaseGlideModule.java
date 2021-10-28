83674b2b6ec1a7d7c03504259ad40ef59946b42c
清竹丶 <ezhuwx@163.com>
2021-10-28 17:29:53 +0800

    1、glide module

---
+++ frame/src/main/java/com/ez/kotlin/frame/base/BaseGlideModule.java
@@ -0,0 +1,86 @@
+package com.ez.kotlin.frame.base;
+
+import static com.ez.kotlin.frame.utils.LogExtKt.logE;
+
+import android.content.Context;
+import android.os.FileUtils;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.engine.cache.DiskCache;
+import com.bumptech.glide.load.engine.cache.DiskLruCacheWrapper;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.AppGlideModule;
+
+import java.io.File;
+import java.io.InputStream;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * @author ezhuwx
+ * {@link GlideModule} implementation to replace Glide's default
+ * {@link java.net.HttpURLConnection} based {@link com.bumptech.glide.load.model.ModelLoader} with an OkHttp based
+ * {@link com.bumptech.glide.load.model.ModelLoader}.
+ * If you're using gradle, you can include this module simply by depending on the aar, the module will be merged
+ * in by manifest merger. For other build systems or for more more information, see
+ * {@link GlideModule}.
+ */
+@GlideModule
+public final class BaseGlideModule extends AppGlideModule {
+
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
+
+
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                // .sslSocketFactory(overLockCard().getSocketFactory())
+                .hostnameVerifier((hostname, session) -> true);
+        registry.append(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(builder.build()));
+    }
+
+    /**
+     * 忽略所有https证书
+     */
+    private SSLContext overLockCard() {
+        final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                X509Certificate[] x509Certificates = new X509Certificate[0];
+                return x509Certificates;
+            }
+        }};
+        try {
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            return sslContext;
+        } catch (Exception e) {
+            logE("ssl出现异常");
+            return null;
+        }
+    }
+
+
+}
